b49e15e02d7f62e95833aa6048c4a7bdcb952611
Rahul Dogra <rahuldogra1998@gmail.com>
2020-12-08 19:11:55 +0530

    stripe setup

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ import Login from "./Login";
 import { auth } from "./firebase";
 import { useStateValue } from "./StateProvider";
 import Payment from "./Payment";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51Hw5R0FNTYSvznUfXMqEjRpvwb1rw73O6H56K8OjJY6bVUEs7Np1oDXbRwX1fmkyaO4SJLQYryL6cEmHtvzlzcof00tyYW4wTq"
+);
 
 function App() {
   const [{}, dispatch] = useStateValue();
@@ -48,7 +54,9 @@ function App() {
           </Route>
           <Route path="/payment">
             <Header />
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Header />
